c59c55d312afd8e7926ee49dd70510af2763bac0
Zhangwai <1519214533@qq.com>
2021-06-05 13:58:57 +0800

    “add cityhot

--- src/components/city/City.vue
+++ src/components/city/City.vue
@@ -1,16 +1,21 @@
 <template>
-    <div>
+    <div class="city">
         <city-header></city-header>
+        <city-hot></city-hot>
     </div>
 </template>
 <script>
     import CityHeader from "./pages/Header"
+    import CityHot from "./pages/Hot"
 export default {
     components:{
-        CityHeader
+        CityHeader,
+        CityHot
     }
 }
 </script>
 <style scoped>
-
+    .city{
+        background: #f5f5f5;
+    }
 </style>
